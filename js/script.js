d1b04c559e402d2ee49d417d605b57ead0b01ed2
gonzalo99 <you@example.com>
2021-11-30 01:25:12 -0300

    Integrando page fichas

--- js/script.js
+++ js/script.js
@@ -48,83 +48,213 @@ const redirect = (url) =>{
     location.href = url;
 }
 
-if (typeof(Storage) !== "undefined") {
-    console.log(1);
-} else {
-    console.log(2);
+
+// Api bombero
+let url = 'https://raw.githubusercontent.com/MrCutux/SGVApp/master/apibomb.json';
+let bomberos = [];
+const peticion = async() =>{
+    const res = await fetch(url);
+    const data = await res.json();
+    //console.log(data);
+    for (const key in data) {
+        if (Object.prototype.hasOwnProperty.call(data, key)) {
+          const arreglo = data[key];
+          mostrarInformacion(arreglo);
+          
+          
+          
+        }
+      }
 }
 
-// Objeto Ficha
-function ficha(rut,dv,nombres,appat,apmat,direc,fechaI,tipoU,comuna){
-    this.rut = rut;
-    this.dv = dv;
-    this.nombres = nombres;
-    this.appat = appat;
-    this.apmat = apmat;
-    this.direc = direc;
-    this.fechaI = fechaI;
-    this.tipoU = tipoU;
-    this.comuna = comuna;
-}
-
-// Arreglo de fichas 
-let fichas = [];
-
-// Funcion agregar ficha
-const agregarFicha = () =>{
-    // Variables
-    let rut = document.getElementById('rut').value;
-    let dv = document.getElementById('dv').value;
-    let nombres = document.getElementById('nombres').value;
-    let appat = document.getElementById('appat').value;
-    let apmat = document.getElementById('apmat').value;
-    let direc = document.getElementById('direc').value;
-    let fechaI = document.getElementById('fechaI').value;
-    let tipoU = document.getElementById('tipoU').value;
-    let comuna = document.getElementById('comuna').value;
+crearFila = (rut,pnom,appat,apmat,direc,ciudad,fecnac,comp,fecin,reg,cargo,estado) =>{
+    return `
     
-    bombero = new ficha(rut,dv,nombres,appat,apmat,direc,fechaI,tipoU,comuna);
-    console.log(bombero)
-    fichas.push(bombero);
-    mostrarDatos(rut,dv,nombres,appat,apmat,direc,fechaI,tipoU,comuna);
-}
-
-
-
-
-const mostrarDatos = (rut,dv,nombres,appat,apmat,direc,fechaI,tipoU,comuna) =>{
-    let tbody = document.getElementById('tbody');
-    
-    tbody.innerHTML += `
     <tr>
-        <td>${rut}</td>
-        <td>${dv}</td>
-        <td>${nombres}</td>
+        <th> <button class="form-control bg-primary btn-list" > ${rut} </button> </th>
+        <td>${pnom}</td>
         <td>${appat}</td>
         <td>${apmat}</td>
         <td>${direc}</td>
-        <td>${fechaI}</td>
-        <td>${tipoU}</td>
-        <td>${comuna}</td>
+        <td>${ciudad}</td>
+        <td>${fecnac}</td>
+        <td>${comp}</td>
+        <td>${fecin}</td>
+        <td>${reg}</td>
+        <td>${cargo}</td>
+        <td>${estado}</td>
         <td>
-        <div class="row">
-            <div class="col">
-            <img title="Actualizar" id="opcion" src="https://cdn-icons.flaticon.com/png/128/2546/premium/2546705.png?token=exp=1637808675~hmac=48c1f6d187a884d3650988a6d02551ba" alt="">
+            <div class="row">
+                 <div class="col">
+                     <img title="Actualizar" class="opcion-table" src="https://cdn-icons-png.flaticon.com/128/942/942748.png" alt="">
+                 </div>
+                 <div class="col">
+                 <img title="Eliminar" class="opcion-table" src="https://cdn-icons-png.flaticon.com/128/3221/3221897.png" alt="">
+                 </div>
+                 
             </div>
-            <div class="col">
-            <img title="Eliminar" id="opcion" src="https://cdn-icons-png.flaticon.com/128/3221/3221897.png" alt=""> 
-            </div>
-        </div>
-       
-        
         </td>
-        
     </tr>
-`
+    
+    `
 }
 
-// Hola 
-// Validar Campos
+/* "rut": "16201197-9",
+"nombre1": "Ignacio",
+"apellido1": "Cuturrufo",
+"apellido2": "Gonzalez",
+"direccion": "Los Nardos 2089",
+"ciudad": "Quilpue",
+"fechanac": "07-05-1986",
+"compañia": "Primera",
+"fechaingreso": "15-12-2005",
+"nreg": "1009",
+"cargo": "Secretario",
+"estado": "Activo", */
+
+
+mostrarInformacion = (a) =>{
+    let array = a;
+    let section = document.getElementById('tbody');
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i].usuario);
+        section.innerHTML += crearFila(array[i].rut,array[i].nombre1,array[i].apellido1,
+            array[i].apellido2,
+            array[i].direccion,
+            array[i].ciudad,
+            array[i].fechanac,
+            array[i].compañia,
+            array[i].fechaingreso,
+            array[i].nreg,
+            array[i].cargo,
+            array[i].estado)
+        bomberos.push(array[i]);
+        
+    }
+}
+peticion();
+
+
+// Modal
+
+
+const mostrarModal = () =>{
+    return `
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Creacion de ficha</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+                <div class="col">
+                    <input id="rut" name="rut" type="text" class="form-control" placeholder="Rut">
+                </div>
+                <div class="col">
+                    <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre">   
+                </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                  <input id="appat" name="appat" type="text" class="form-control" placeholder="Apellido Paterno">
+              </div>
+              <div class="col">
+                  <input type="text" id="apmat" name="apmat" class="form-control" placeholder="Apellido Materno">   
+              </div>
+              </div>
+              <div class="row">
+                  <div class="col">
+                      <input id="direc" name="direc" type="text" class="form-control" placeholder="Direccion">
+                  </div>
+                  <div class="col">
+                      <input title="Fecha nacimiento" type="date" id=fecnac" name="fecnac" class="form-control" >   
+                  </div>
+              </div>
+              <div class="row">
+                  <div class="col">
+                      <input id="compa" name="compa" type="text" class="form-control" placeholder="Compañia">
+                  </div>
+                  <div class="col">
+                      <input title="Fecha ingreso" type="date" id=feciin" name="feciin" class="form-control" >   
+                  </div>
+              </div>
+              <div class="row">
+                  <div class="col">
+                      <input id="nroreg" name="nroreg" type="text" class="form-control" placeholder="Nro registro">
+                  </div>
+                  <div class="col">
+                      <input type="text" id=cargo" name="cargo" class="form-control" placeholder="Cargo">   
+                  </div>
+                  <div class="col">
+                      <input type="text" id=estado" name="estado" class="form-control" placeholder="Estado">   
+                  </div>
+              </div>
+              <div id="div-msg">
+              
+              </div>
+              
+  
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-success" data-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-primary" onclick="mensajeDiv()">Crear</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+    
+    `
+}
+
+const modalMensaje = () =>{
+    return `
+    <!-- Modal -->
+    <div class="modal fade" id="mensaje" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    `
+}
+
+const agregarModal = () =>{
+    let body = document.getElementById('body');
+    body.innerHTML += mostrarModal(); 
+}
+
+const mensajeDiv = () =>{
+    let div = document.getElementById('div-msg');
+    div.innerHTML = validarMensaje();
+}
+
+const validarMensaje = () =>{
+    return `
+    <p class="alert-success p-msg">Ficha creada exitosamente</p>
+    
+    `
+}
 
 
 
